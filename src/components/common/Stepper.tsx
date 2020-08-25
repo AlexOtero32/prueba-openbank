80008ce3824ddce822287ea7b7508501fd06448c
AlexOtero32 <alex.otero-94@hotmail.com>
2020-08-25 12:57:53 +0200

    Añadida funcionalidad a Stepper

--- src/components/common/Stepper.tsx
+++ src/components/common/Stepper.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import classnames from 'classnames';
 import { FaCheck } from 'react-icons/fa';
+import { DisplayingScreen } from '../../redux/ui/types';
+import { RootState } from '../../redux/store';
+import { connect, ConnectedProps } from 'react-redux';
 
-type StepperProps = {
-    step: number;
-    current: boolean;
-    completed: boolean;
+type StepperProps = StepperReduxProps & {
+    step: DisplayingScreen;
 };
 
 const Stepper: React.FC<StepperProps> = props => {
+    const completed = props.step < props.screen;
+
     return (
         <span
             className={classnames('stepper', {
-                'stepper-done': props.completed,
-                'stepper-current': props.current,
+                'stepper-done': completed,
+                'stepper-current': props.screen === props.step,
             })}
         >
-            {props.completed ? <FaCheck /> : props.step}
+            {completed ? <FaCheck /> : props.step + 1}
         </span>
     );
 };
 
-export default Stepper;
+const mapStateToProps = (state: RootState) => ({
+    screen: state.ui.screen,
+});
+
+const connector = connect(mapStateToProps);
+
+type StepperReduxProps = ConnectedProps<typeof connector>;
+
+export default connector(Stepper);
--- src/components/wizard/WizardHeader.tsx
+++ src/components/wizard/WizardHeader.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Stepper from '../common/Stepper';
+import { DisplayingScreen } from '../../redux/ui/types';
 
 const WizardHeader: React.FC<{}> = () => {
     return (
         <header className="wizard-header">
-            <Stepper step={1} current={false} completed={true} />
-            <Stepper step={2} current={true} completed={false} />
-            <Stepper step={3} current={false} completed={false} />
+            <Stepper step={DisplayingScreen.ProductInformation} />
+            <Stepper step={DisplayingScreen.Form} />
+            <Stepper step={DisplayingScreen.Feedback} />
         </header>
     );
 };
